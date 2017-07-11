2c2b9be6dc58cfe4729bcf0f427d14138bddafd4
Kasper Halme <kasper.halme@futurice.com>
2017-07-11 10:36:30 +0300

    add Carousel and specs

---
+++ src/Carousel.elm
@@ -0,0 +1,107 @@
+module Carousel exposing (..)
+
+
+type alias Carousel a =
+    { previous : a
+    , current : a
+    , next : a
+    , rest : List a
+    }
+
+
+{-|
+    onwards { previous = 1, current = 2, next = 3, rest = []}
+    --> { previous = 2, current = 3, next = 1, rest = []}
+
+    onwards { previous = 1, current = 2, next = 3, rest = [4, 5, 6]}
+    --> { previous = 2, current = 3, next = 4, rest = [5, 6, 1]}
+
+-}
+onwards : Carousel a -> Carousel a
+onwards { previous, current, next, rest } =
+    let
+        newNext =
+            rest |> List.head |> Maybe.withDefault previous
+
+        newRest =
+            case rest of
+                [] ->
+                    []
+
+                head :: tail ->
+                    tail ++ [ previous ]
+    in
+        { previous = current
+        , current = next
+        , next = newNext
+        , rest = newRest
+        }
+
+
+{-|
+    backwards { previous = 1, current = 2, next = 3, rest = []}
+    --> { previous = 3, current = 1, next = 2, rest = []}
+
+    backwards { previous = 1, current = 2, next = 3, rest = [4, 5, 6]}
+    --> { previous = 6, current = 1, next = 2, rest = [3, 4, 5]}
+
+-}
+backwards : Carousel a -> Carousel a
+backwards { previous, current, next, rest } =
+    let
+        newPrevious =
+            rest |> List.reverse |> List.head |> Maybe.withDefault next
+
+        newRest =
+            case List.reverse rest of
+                [] ->
+                    []
+
+                head :: tail ->
+                    next :: List.reverse tail
+    in
+        { previous = newPrevious
+        , current = previous
+        , next = current
+        , rest = newRest
+        }
+
+
+{-|
+    fromList "Meera" []
+    --> Nothing
+
+    fromList "Meera" [ "Cersei", "Hodor", "Joffrey"]
+    --> Nothing
+
+    fromList "Meera" [ "Cersei", "Hodor", "Joffrey", "Meera" ]
+    --> Just { previous = "Joffrey", current = "Meera", next = "Cersei", rest = ["Hodor"] }
+
+-}
+fromList : a -> List a -> Result String (Carousel a)
+fromList needle list =
+    let
+        length =
+            List.length list
+
+        recurse remainingRounds needle list =
+            if remainingRounds <= 0 then
+                Err "not found"
+            else
+                case list of
+                    [] ->
+                        Err "list was too short"
+
+                    _ :: [] ->
+                        Err "list was too short"
+
+                    _ :: _ :: [] ->
+                        Err "list was too short"
+
+                    prev :: curr :: next :: rest ->
+                        if curr == needle then
+                            Ok { previous = prev, current = curr, next = next, rest = rest }
+                        else
+                            recurse (remainingRounds - 1) needle (curr :: next :: rest ++ [ prev ])
+    in
+        recurse length needle list
--- src/Pages/Project.elm
+++ src/Pages/Project.elm
@@ -2,33 +2,75 @@ module Pages.Project exposing (..)
 
 import Html exposing (..)
 import Msgs exposing (Msg)
-import Models exposing (Project)
+import Models exposing (Project, initialModel)
 import Styles exposing (..)
 import Pages.Home exposing (header)
 import Html.Attributes exposing (class, value, href)
 import Routing.Router exposing (projectPath)
+import RemoteData exposing (WebData)
+import Carousel exposing (Carousel, fromList)
 
 
-view : Project -> Html Msg
-view model =
+view : Project -> WebData (List Project) -> Html Msg
+view project projects =
     div []
-        [ form model
+        [ maybeList project projects
         ]
 
 
-form : Project -> Html Msg
-form project =
-    div []
-        [ div [ styles flexContainer ]
-            [ Pages.Home.header ]
-        , div [ styles flexContainer ]
-            [ Pages.Home.secondaryHeader project ]
-        , div [ styles projectContainer ]
-            [ btnPrevious project
-            , projectItem project
-            , btnNext project
+maybeList : Project -> WebData (List Project) -> Html Msg
+maybeList project response =
+    case response of
+        RemoteData.NotAsked ->
+            text ""
+
+        RemoteData.Loading ->
+            text "Loading..."
+
+        RemoteData.Success projects ->
+            form project projects
+
+        RemoteData.Failure error ->
+            text (toString error)
+
+
+testItem : String -> Html Msg
+testItem testValue =
+    let
+        path =
+            projectPath testValue
+    in
+        a [ href path ]
+            [ text testValue
             ]
-        ]
+
+
+form : Project -> List Project -> Html Msg
+form project projects =
+    let
+        carouselList =
+            (List.map (\a -> a.id) projects)
+
+        carouselView =
+            case fromList project.id carouselList of
+                Err message ->
+                    div []
+                        [ text message ]
+
+                Ok carousel ->
+                    div [ styles projectContainer ]
+                        [ btnPrevious carousel.previous
+                        , projectItem project
+                        , btnNext carousel.next
+                        ]
+    in
+        div []
+            [ div [ styles flexContainer ]
+                [ Pages.Home.header ]
+            , div [ styles flexContainer ]
+                [ Pages.Home.secondaryHeader project ]
+            , carouselView
+            ]
 
 
 projectItem : Project -> Html Msg
@@ -51,26 +93,23 @@ projectItem project =
         ]
 
 
-stringToInt : String -> Int
-stringToInt str =
-    Result.withDefault 0 (String.toInt str)
-
-
-btnPrevious : Project -> Html Msg
+btnPrevious : String -> Html Msg
 btnPrevious project =
     let
         path =
-            projectPath (toString ((stringToInt project.id) - 1))
+            projectPath project
     in
         a [ class "btn ml1 h1", href path ]
             [ i [ class "fa fa-caret-left" ] [] ]
 
 
-btnNext : Project -> Html Msg
+btnNext : String -> Html Msg
 btnNext project =
     let
         path =
-            projectPath (toString ((stringToInt project.id) + 1))
+            projectPath project
     in
-        a [ class "btn ml1 h1", href path ]
-            [ i [ class "fa fa-caret-right" ] [] ]
+        a
+            [ class "btn ml1 h1", href path ]
+            [ i [ class "fa fa-caret-right" ] []
+            ]
